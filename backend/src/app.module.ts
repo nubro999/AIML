2aa90383fa11fa921e4ee8839d0e249a185490b8
chris <aliveck0208@gmail.com>
2024-09-27 16:25:46 +0900

    backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,26 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MinaModule } from './mina/mina.module';
-import { PumpModule } from './pump/pump.module';
-import { PumpController } from './pump/pump.controller';
-import { PumpService } from './pump/pump.service';
-import { UserModule } from './user/user.module';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Pump } from './pump/pump.entity';
-import { PumpRepository } from './pump/pump.repository';
-import { PricefeedModule } from './pricefeed/pricefeed.module';
+
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    host: 'ezcryptoexchange.creye0kuc4ct.ap-northeast-2.rds.amazonaws.com',
+    host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'rootroot',
-    database: 'ezcryptoexchange',
-    entities: [Pump],
+    password: 'root',
+    database: 'silentauction',
+    entities: [],
     synchronize: true,
-  }), MinaModule, PumpModule, UserModule, PricefeedModule],
+  }), MinaModule, ],
   controllers: [],
   providers: [],
 })
--- backend/src/pricefeed/pricefeed.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PriceFeed } from './pricefeed';
-
-@Module({
-
-    providers: [PriceFeed]
-})
-export class PricefeedModule {}
