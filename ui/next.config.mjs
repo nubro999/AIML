e4968fd44e9dd334cae1e9bb3597f4a5b05c6418
chris <aliveck0208@gmail.com>
2024-10-28 23:03:06 +0900

    remove_eslint

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
     unoptimized: true, // Required for static export
   },
 
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.alias = {
